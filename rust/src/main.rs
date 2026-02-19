6103af8239b6583e7363ca468c36918270bb20d2
Anusuya-murugiah <133192318+Anusuya-murugiah@users.noreply.github.com>
2026-02-19 14:42:02 +0530

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -21,7 +21,7 @@ struct Options<'r> {
 //   http://127.0.0.1:8000/hello/world
 #[get("/world")]
 fn world() -> &'static str {
-    "Hello, world! Welcome everyone"
+    "Hello, world! Welcome everyone!"
 }
 
 // Try visiting:
